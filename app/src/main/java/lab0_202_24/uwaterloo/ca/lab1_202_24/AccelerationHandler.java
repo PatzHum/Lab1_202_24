f3ce1c97eb290e09dd0f54c02dc9cbead40b9d26
PatzHum <patrick.hum@hotmail.com>
2017-01-18 10:09:25 -0500

    Updated Handler

--- app/src/main/java/lab0_202_24/uwaterloo/ca/lab1_202_24/AccelerationHandler.java
+++ app/src/main/java/lab0_202_24/uwaterloo/ca/lab1_202_24/AccelerationHandler.java
@@ -1,6 +1,10 @@
 package lab0_202_24.uwaterloo.ca.lab1_202_24;
 
+import android.content.Context;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 /**
  * Created by patri on 2017-01-18.
@@ -8,14 +12,29 @@ import android.widget.TextView;
 
 public class AccelerationHandler extends SensorHandler {
 
-    AccelerationHandler(TextView OutputTextView){
-        super(OutputTextView);
+    float[] gravity = new float[3];
+
+    AccelerationHandler(Context applicationContext, LinearLayout layout, String sensorType){
+        super(applicationContext, layout, sensorType);
     }
 
     @Override
-    protected void HandleOutput(float[] values){
+    protected float[] ProcessData(float[] values){
         UpdateMaxValues(values);
         // Format Output...
+
+        float alpha = (float) 0.8;
+
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * values[2];
+
+        float[] acc = new float[3];
+        acc[0] = values[0] - gravity[0];
+        acc[1] = values[1] - gravity[1];
+        acc[2] = values[2] - gravity[2];
+
+        return acc;
     }
 
 
--- app/src/main/java/lab0_202_24/uwaterloo/ca/lab1_202_24/SensorHandler.java
+++ app/src/main/java/lab0_202_24/uwaterloo/ca/lab1_202_24/SensorHandler.java
@@ -1,5 +1,7 @@
 package lab0_202_24.uwaterloo.ca.lab1_202_24;
 
+import android.content.Context;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Vector;
@@ -9,11 +11,23 @@ import java.util.Vector;
  */
 
 public abstract class SensorHandler {
-    TextView mOutputTextView;
+    TextView mOutputLabel, mOutputValue, mMaxOutputLabel, mMaxOutputValue;
     Vector<Float> mMaximumValues;
 
-    SensorHandler(TextView OutputTextView){
-        mOutputTextView = mOutputTextView;
+    SensorHandler(Context applicationContext, LinearLayout layout, String sensorType){
+        mOutputLabel = new TextView(applicationContext);
+        mOutputValue = new TextView(applicationContext);
+        mMaxOutputLabel = new TextView(applicationContext);
+        mMaxOutputValue = new TextView(applicationContext);
+
+        layout.addView(mOutputLabel);
+        layout.addView(mOutputValue);
+        layout.addView(mMaxOutputLabel);
+        layout.addView(mMaxOutputValue);
+
+
+        mOutputLabel.setText(String.format("The %s sensor reading is: "));
+        mMaxOutputLabel.setText(String.format("The maximum %s reading is: "));
     }
 
     protected void UpdateMaxValues(float[] values){
@@ -22,5 +36,31 @@ public abstract class SensorHandler {
                 mMaximumValues.set(i, values[i]);
         }
     }
-    abstract protected void HandleOutput(float[] values);
+
+    protected float[] ProcessData(float[] values){
+        return values;
+    }
+
+    public void HandleOutput(float[] values){
+        values = ProcessData(values);
+
+        UpdateMaxValues(values);
+
+        String outputValueString = "";
+        for (int i = 0; i < values.length; ++i){
+            if (i != 0){
+                outputValueString += ",";
+            }
+            outputValueString += Float.toString(values[i]);
+        }
+        mOutputValue.setText(outputValueString);
+
+        String outputMaxValueString = "";
+        for (int i = 0; i < mMaximumValues.size(); ++i){
+            if (i != 0){
+                outputMaxValueString += ",";
+            }
+            outputMaxValueString += Float.toString(mMaximumValues.get(i));
+        }
+    }
 }
